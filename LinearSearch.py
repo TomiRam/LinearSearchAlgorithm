dc91be4fcc1f8362c5eba3160a7d35ee1548aad2
Tomi Rämö <47462643+TomiRam@users.noreply.github.com>
2020-08-18 13:10:51 +0300

    Add files via upload

---
+++ LinearSearch.py
@@ -0,0 +1,30 @@
+from datetime import datetime
+
+def liner_search (lst, x);
+	i=0
+	length = len(lst)
+	while i< length;
+		if lst[i] == x:
+			return i
+		i += 1
+
+	return None
+
+items = [2,3,,5,7,11,13,17]
+print(liner_search(items, 1)) #None
+print(liner_search(items, 7) # 3
+print(liner_search(items, 19)) #None
+
+#Simplfied speed test
+items = range(0,100000)
+count = 100
+
+start = datetime.now()
+
+for i in range(1, count):
+	liner_search(items,777777)
+
+delta = datetime.now() -start
+totalMicroseconds = delta.seconds*1000000
+
+print(totalMicroseconds / count)
